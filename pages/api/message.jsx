136bb2f1f9425eeb6f8d5102a896a8a84815579e
TJ <xxterrelljacksonxx@gmail.com>
2023-05-30 08:21:18 -0400

    update

--- pages/api/message.jsx
+++ pages/api/message.jsx
@@ -1,26 +1,28 @@
 export default async function ContactAPI(req, res) {
   console.log(req.body);
+  console.log(process.env.user, process.env.pass)
+  res.status(200).json({ message: "success" });
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.user,
-      pass: process.env.pass,
-    },
-  });
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp.gmail.com",
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     user: process.env.user,
+  //     pass: process.env.pass,
+  //   },
+  // });
 
-  try {
-    const mail = await transporter.sendMail({
-      
-    });
+  // try {
+  //   const mail = await transporter.sendMail({
 
-    res.status(200).json({ message: "success" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Your message could not be sent.",
-    });
-  }
+  //   });
+
+  //   res.status(200).json({ message: "success" });
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(500).json({
+  //     message: "Your message could not be sent.",
+  //   });
+  // }
 }
