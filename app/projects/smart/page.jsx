c6b7571ee692531a37019f325b1b5fc8a1d0f945
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2024-07-17 10:30:31 -0400

    updated smart brain live site url
    Deployed project to Vercel to improve speed.

--- app/projects/smart/page.jsx
+++ app/projects/smart/page.jsx
@@ -40,7 +40,7 @@ const Smart = () => {
           <a href="https://tjrelly.github.io/smart-brain/" target="_blank">
             <button className="px-8 py-2 mt-6 mr-5">Live Site</button>
           </a>
-          <a href="https://github.com/tjrelly/smart-brain" target="_blank">
+          <a href="https://smart-brain-chi.vercel.app/" target="_blank">
             <button className="px-8 py-2 mt-6">Github Repo</button>
           </a>
         </div>
